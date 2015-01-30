4d3182d7c955d5a7bb6e1088dfea35af05f653ac
AlexLai1990 <xincheng.lai@gmail.com>
2015-01-30 12:41:42 -0800

    minWindow

--- Array String -Hashtable/MinumumWindowSubstring.cpp
+++ Array String -Hashtable/MinumumWindowSubstring.cpp
@@ -3,39 +3,41 @@
    When it comes to right size, update the minimum window.
  */
 
-string minWindow(string S, string T) {
-    if (S.size() < 1 || S.size() < T.size())
-        return "";
-    int expect[256];
-    int appear[256];
-    memset(expect, 0, sizeof(expect));
-    memset(appear, 0, sizeof(appear));
-    int left = 0, right = 0;
-    int count = 0;
-    int min_window = INT_MAX;
-    int min_start = 0;
-    for (int i = 0; i < T.size(); i++) {
-        expect[T[i] - '\0']++;
-    }
-    int i = 0;
-    while (i <= S.size() - 1) {
-        if (expect[S[i] - '\0'] > 0) {
+class Solution {
+public:
+    string minWindow(string S, string T) {
+        if (S.size() < 1 || S.size() < T.size())
+            return "";
+        int expect[256];
+        int appear[256];
+        memset(expect, 0, 256 * sizeof(int));
+        memset(appear, 0, 256 * sizeof(int));
+        int left = 0, right = 0;
+        int count = 0;
+        int min_window = INT_MAX;
+        int min_start = 0;
+        for (int i = 0; i < T.size(); i++) {
+            expect[T[i] - '\0']++;
+        }
+        int i = 0;
+        while (i <= S.size() - 1) {
+            if (appear[S[i] - '\0'] < expect[S[i] - '\0'])
+                count++; 
             appear[S[i] - '\0']++;
-            if (appear[S[i] - '\0'] <= expect[S[i] - '\0'])
-                count++;
-        }
-        if (count == T.size()) {
-            right = i;
-            while(appear[S[left] - '\0'] > expect[S[left] - '\0'] || expect[S[left] - '\0'] == 0) {
-                appear[S[left] - '\0']--;
-                left++;
+            // find the min_windows only when it meets the need.
+            if (count == T.size()) {
+                right = i;
+                while(appear[S[left] - '\0'] > expect[S[left] - '\0'] ) {
+                    appear[S[left] - '\0']--;
+                    left++;
+                }
+                if (min_window > right - left + 1) {
+                    min_window = right - left + 1;
+                    min_start = left;
+                }
             }
-            if (min_window > right - left + 1) {
-                min_window = right - left + 1;
-                min_start = left;
-            }
+            i++;
         }
-        i++;
+        return min_window == INT_MAX ? "" : S.substr(min_start, min_window);
     }
-    return min_window == INT_MAX ? "" : S.substr(min_start, min_window);
-}
+};
